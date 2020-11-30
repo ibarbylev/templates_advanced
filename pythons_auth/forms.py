1f3d04b8d48e592c6581479fa617e8a3f9c42f4f
su <admin>
2020-11-30 18:30:25 +0200

    add django-bootstrap4

---
+++ pythons_auth/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', False)
+        if not email:
+            raise forms.ValidationError('Email is required!')
+        return email
--- pythons_auth/views.py
+++ pythons_auth/views.py
@@ -1,5 +1,23 @@
 from django.contrib.auth import logout, authenticate, login
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+
+from pythons_auth.forms import RegisterForm
+
+
+def register_user(request):
+    if request.method == 'GET':
+        context = {'form': RegisterForm()}
+        return render(request, 'auth/register.html', context)
+    else:
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+
+        context = {'form': form}
+        return render(request, 'auth/register.html', context)
+
 
 
 def login_user(request):
