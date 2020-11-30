e62657f9d1bb96e9e5369257e5b61b9bce4a5804
su <admin>
2020-11-30 22:31:53 +0200

    add register form with python-lib bootstrap4

--- pythons_auth/forms.py
+++ pythons_auth/forms.py
@@ -2,12 +2,19 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from pythons_auth.models import UserProfile
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
 
 # class RegisterForm(forms.ModelForm):
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'password', 'email')
+        fields = ('username', 'email')
         widgets = {
             'password': forms.PasswordInput(),
         }
@@ -17,3 +24,9 @@ class RegisterForm(UserCreationForm):
         if not email:
             raise forms.ValidationError('Email is required!')
         return email
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
--- pythons_auth/views.py
+++ pythons_auth/views.py
@@ -1,26 +1,42 @@
 from django.contrib.auth import logout, authenticate, login
+from django.db import transaction
 from django.shortcuts import redirect, render
 
-from pythons_auth.forms import RegisterForm
+from pythons_auth.forms import RegisterForm, ProfileForm, LoginForm
 
 
+@transaction.atomic
 def register_user(request):
     if request.method == 'GET':
-        context = {'form': RegisterForm()}
+        context = {
+            'user_form': RegisterForm(),
+            'profile_form': ProfileForm(),
+        }
         return render(request, 'auth/register.html', context)
     else:
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
+        user_form = RegisterForm(request.POST)
+        profile_form = ProfileForm(request.POST, request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
             login(request, user)
             return redirect('index')
 
-        context = {'form': form}
+        context = {
+            'user_form': RegisterForm(request.POST),
+            'profile_form': ProfileForm(request.POST, request.FILES),
+        }
         return render(request, 'auth/register.html', context)
 
 
-
 def login_user(request):
+    if request.method == 'GET':
+        context = {'login_form': LoginForm()}
+
+        return render(request, 'auth/login.html', context)
     username = 'root'
     password = '123'
     user = authenticate(username=username, password=password)
