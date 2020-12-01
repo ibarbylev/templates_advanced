d129adc183cb6527df8fb1fd656286693a88b897
su <admin>
2020-12-01 04:29:59 +0200

    add redirect where need after login

--- pythons_auth/views.py
+++ pythons_auth/views.py
@@ -33,6 +33,11 @@ def register_user(request):
         return render(request, 'auth/register.html', context)
 
 
+def get_redirect_url(params):
+    redirect_url = params['return_url']
+    return redirect_url if redirect_url else 'index'
+
+
 def login_user(request):
     if request.method == 'GET':
         context = {'login_form': LoginForm()}
@@ -40,13 +45,14 @@ def login_user(request):
         return render(request, 'auth/login.html', context)
     else:
         login_form = LoginForm(request.POST)
+        return_url = get_redirect_url(request.POST)  # where need redirect after login
         if login_form.is_valid():
             username = login_form.cleaned_data['username']
             password = login_form.cleaned_data['password']
             user = authenticate(username=username, password=password)
             if user:
                 login(request, user)
-                return redirect('index')
+                return redirect(return_url)
 
         context = {'login_form': login_form}
         return render(request, 'auth/login.html', context)
