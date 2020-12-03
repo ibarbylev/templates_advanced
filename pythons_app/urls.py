5feac442271e50b9f6f556aadaa2276353a49b04
su <admin>
2020-12-03 16:24:52 +0200

    lab: transform index_view() to IndexListView(ListView)

--- pythons_app/urls.py
+++ pythons_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    # path('', views.index, name="index"),
+    path('', views.IndexListView.as_view(), name="index"),
     path('create/', views.create, name="create")
 ]
--- pythons_app/views.py
+++ pythons_app/views.py
@@ -1,15 +1,23 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.views.generic import ListView
 
 from pythons_core.decorators import group_required
 from .forms import PythonCreateForm
 from .models import Python
 
 
-@login_required
-def index(req):
-    pythons = Python.objects.all()
-    return render(req, 'index.html', {'pythons': pythons})
+# @login_required
+# def index(req):
+#     pythons = Python.objects.all()
+#     return render(req, 'index.html', {'pythons': pythons})
+
+
+# @login_required
+class IndexListView(ListView):
+    model = Python
+    context_object_name = 'pythons'
+    template_name = 'index.html'
 
 
 @group_required(groups=['User'])
